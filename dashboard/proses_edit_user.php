50dcb1810fe3fc70b8ec69e9a01163dc12dd1823
reva azzahra <sheisrevaazzahra@gmail.com>
2026-06-18 17:24:04 +0700

    selesai

---
+++ dashboard/proses_edit_user.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../database.php";
+require_once "../user.php";
+
+$username = $_POST["username"];
+$email = $_POST["email"];
+$asal = $_POST["asal"];
+$password = $_POST["password"];
+
+$database = new Database;
+$koneksi_db = $database->koneksi();
+
+$user = new User($koneksi_db);
+$user->update($id, $username, $email, $asal, $password);
